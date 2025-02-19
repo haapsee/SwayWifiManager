ba0ae6106bcd627ce25600c0da1101e170b21b32
Eemil Haapsaari <eemil@identio.fi>
2025-02-19 18:56:12 +0200

    Hello World

---
+++ build/CMakeFiles/SwayWifiManager.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SwayWifiManager.dir/src/main.cpp.o"
+  "CMakeFiles/SwayWifiManager.dir/src/main.cpp.o.d"
+  "SwayWifiManager"
+  "SwayWifiManager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SwayWifiManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
